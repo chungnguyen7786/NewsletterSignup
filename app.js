df243bcecc0afd08db4bb9d64e1906a2ffc6a129
Chung Nguyen <themask7786@gmail.com>
2021-12-27 11:47:28 +0700

    first commit with node-express backbone

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
+});
